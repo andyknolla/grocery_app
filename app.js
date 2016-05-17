fbf51d4616b265708d522892c8a006d376d33987
Andy Knolla <andyknolla@gmail.com>
2016-05-17 09:32:46 -0600

    commit before implementing sass

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-$(function() {
-    $( "#tabs" ).tabs();
+  $(function() {
+    $( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+    $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
   });
-
   });
