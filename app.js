4d9fac1ed3a0ce1346fb9a3e9a23495d32480204
Andy Knolla <andyknolla@gmail.com>
2016-05-17 12:30:21 -0600

    add bootstrap, including tab feature

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
 
-  $(function() {
-    $( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
-    $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
-  });
-  });
+$('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+$('.catButton').on('click', function() {
+  var dirka = $('table').find('tr:first');
+  var itemCategory = $('table:last-child:nth-child(2)')
+  var itemPrice = $('table:last-child:nth-child(3)')
+  var itemQty = $('table:last-child:nth-child(4)')
+
+})
+
+
+
+
+
+
+
+
+
+});
