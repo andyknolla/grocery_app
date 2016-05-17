dc640c2f50668551ac3d3ad982e070ced861dc8f
Andy Knolla <andyknolla@gmail.com>
2016-05-17 17:26:43 -0600

    add functions for addNewItem button.  takes form inputs, places them into table

--- app.js
+++ app.js
@@ -14,38 +14,57 @@ var categoryBox10 = $('.categoryBox').find('button').eq(9).val();
 var categoryBox11= $('.categoryBox').find('button').eq(10).val();
 var categoryBox12 = $('.categoryBox').find('button').eq(11).val();
 
-// declare variables for form inputs
-var itemNameVal = $('#itemName').val();
-var itemPriceVal = $('#itemPrice').val();
-var itemQtyVal = $('#itemQty').val();
-var categoryVal = $('#category').val();
+
 
 // declare variables for table elements
 var tableRow = $('table').find('tr:last');
-  var newTableRowName = tableRow.find('td').eq(0).text();
+  //var newTableRowName = tableRow.find('td').eq(0).text(); // set text = formVal variables on "submit"
   var newTableRowCategory = tableRow.find('td').eq(1).text();
   var newTableRowPrice = tableRow.find('td').eq(2).text();
   var newTableRowQty = tableRow.find('td').eq(3).text();
-  var newTableRowTotalPrice = function() {
-    return newTableRowPrice * newTableRowQty;
-  }
+  var newTableRowTotalPrice;
 
 $(document).ready(function() {
 
-$('#myTabs a').click(function (e) {
-  e.preventDefault()
+$('#myTabs a').click(function (event) {
+  event.preventDefault()
   $(this).tab('show')
+})
+
+$('.categoryBox').on('click', 'button', function(event) {
+  event.preventDefault();
+console.log( $(this).val() );
+  $('#category').val( $(this).val() );
+
 })
 
 
 // take form values, put them into table tr elements
-$('#addItemButton').on('click', function() {
-  var $dirka = $('table').find('tr:first').children()[0]; // returns name tr
+$('#addItemButton').on('click', function(event) {
+  event.preventDefault()
+  // declare variables for form inputs
+  var itemNameVal = $('#itemName').val();
+  var itemPriceVal = $('#itemPrice').val();
+  var itemQtyVal = $('#itemQty').val();
+  var categoryVal = $('#category').val();
+
+console.log(itemNameVal);
+tableRow.find('td').eq(0).text(categoryVal);
+tableRow.find('td').eq(1).text(itemNameVal);
+tableRow.find('td').eq(2).text(itemPriceVal);
+tableRow.find('td').eq(3).text(itemQtyVal);
+
+newTableRowTotalPrice = function(num1, num2) {
+  return num1 * num2;
+}
+console.log(newTableRowTotalPrice);
+tableRow.find('td').eq(4).text(newTableRowTotalPrice(itemPriceVal, itemQtyVal));
+
+var nextTableRow = $('<tr><td>dirka</td><td>dirka</td><td></td><td></td><td></td></tr>');  // create new table row and put in variable
+$('tbody').append(nextTableRow);  // append new blank row
+tableRow = $('table').find('tr:last');  // reassign tableRow to current last row
 
 
-  var itemCategory = $('table:last-child:nth-child(2)')
-  var itemPrice = $('table:last-child:nth-child(3)')
-  var itemQty = $('table:last-child:nth-child(4)')
 
 })
 
