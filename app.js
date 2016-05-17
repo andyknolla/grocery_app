7d83ba4612b77d4cb238ed5a2a9bd322355df1a9
Andy Knolla <andyknolla@gmail.com>
2016-05-17 14:35:02 -0600

    declare variables using jQuery

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+
+// declare variables for categoryBox buttons
+
+var categoryBox1 = $('.categoryBox').find('button').eq(0).val();
+var categoryBox2 = $('.categoryBox').find('button').eq(1).val();
+var categoryBox3 = $('.categoryBox').find('button').eq(2).val();
+var categoryBox4 = $('.categoryBox').find('button').eq(3).val();
+var categoryBox5 = $('.categoryBox').find('button').eq(4).val();
+var categoryBox6 = $('.categoryBox').find('button').eq(5).val();
+var categoryBox7 = $('.categoryBox').find('button').eq(6).val();
+var categoryBox8 = $('.categoryBox').find('button').eq(7).val();
+var categoryBox9 = $('.categoryBox').find('button').eq(8).val();
+var categoryBox10 = $('.categoryBox').find('button').eq(9).val();
+var categoryBox11= $('.categoryBox').find('button').eq(10).val();
+var categoryBox12 = $('.categoryBox').find('button').eq(11).val();
+
+// declare variables for form inputs
+var itemNameVal = $('#itemName').val();
+var itemPriceVal = $('#itemPrice').val();
+var itemQtyVal = $('#itemQty').val();
+var categoryVal = $('#category').val();
+
+// declare variables for table elements
+var tableRow = $('table').find('tr:last');
+  var newTableRowName = tableRow.find('td').eq(0).text();
+  var newTableRowCategory = tableRow.find('td').eq(1).text();
+  var newTableRowPrice = tableRow.find('td').eq(2).text();
+  var newTableRowQty = tableRow.find('td').eq(3).text();
+  var newTableRowTotalPrice = function() {
+    return newTableRowPrice * newTableRowQty;
+  }
+
 $(document).ready(function() {
 
 $('#myTabs a').click(function (e) {
@@ -5,8 +37,12 @@ $('#myTabs a').click(function (e) {
   $(this).tab('show')
 })
 
-$('.catButton').on('click', function() {
-  var dirka = $('table').find('tr:first');
+
+// take form values, put them into table tr elements
+$('#addItemButton').on('click', function() {
+  var $dirka = $('table').find('tr:first').children()[0]; // returns name tr
+
+
   var itemCategory = $('table:last-child:nth-child(2)')
   var itemPrice = $('table:last-child:nth-child(3)')
   var itemQty = $('table:last-child:nth-child(4)')
@@ -17,6 +53,7 @@ $('.catButton').on('click', function() {
 
 
 
+//json object
 
 
 
